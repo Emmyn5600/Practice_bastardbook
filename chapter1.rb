b0e0dced6d2b50dc707720bcb3135ef37f01c363
emmy <emmy66418@gmail.com>
2021-04-24 17:26:30 +0200

    Add loop example

--- chapter1.rb
+++ chapter1.rb
@@ -73,3 +73,22 @@ compiled_file.write(k2.read)
 k1.close
 k2.close
 compiled_file.close
+
+#Loops and collection
+
+(1..10).each do |a_number|
+    if a_number.even?
+        puts "the even number #{a_number}"
+    else
+        puts "the odd number #{a_number}"
+    end
+end
+
+# puts( 10 + "42") strings and numbers cannot be added together. 
+#puts( 10.to_s + "42") we can use the to_s method to convert the number into a string
+
+remote_base_url = "http://en.wikipedia.org/wiki"
+(1..3).each do |some_number|
+    r_url = remote_base_url + "/" + some_number.to_s
+    puts r_url
+end  
